f792f5c5ec4dbb6cd67d54cac570d8e05c303053
nealt7558 <71712119+nealt7558@users.noreply.github.com>
2026-02-02 16:02:15 -0500

    Added location references

---
+++ Tic Tac Toe/locations.py
@@ -0,0 +1,97 @@
+home_position = {
+    "base": 1550,
+    "shoulder": 1450,
+    "elbow": 1450,
+    "wrist": 1500,
+    "rotate": 1400,
+    "gripEmpty": 1000,
+    "gripHolding": 1300
+}
+
+first_cube = {
+    "base": 1150,
+    "shoulder": 1350,
+    "elbow": 1450,
+    "wrist": 1600,
+    "rotate": 1400,
+}
+
+# Top
+
+top_left = {
+    "base": 1750,
+    "shoulder": 1700,
+    "elbow": 1800,
+    "wrist": 1500,
+    "rotate": 1400,
+}
+
+top_center = {
+    "base": 1500,
+    "shoulder": 1800,
+    "elbow": 1900,
+    "wrist": 1500,
+    "rotate": 1400,
+}
+
+top_right = {
+    "base": 1250,
+    "shoulder": 1800,
+    "elbow": 1900,
+    "wrist": 1500,
+    "rotate": 1400,
+}
+
+
+
+# Center
+center_left = {
+    "base": 1680,
+    "shoulder": 1500,
+    "elbow": 1650,
+    "wrist": 1500,
+    "rotate": 1400,
+}
+
+
+center_center = {
+    "base": 1550,
+    "shoulder": 1500,
+    "elbow": 1650,
+    "wrist": 1500,
+    "rotate": 1400,
+}
+
+center_right = {
+    "base": 1350,
+    "shoulder": 1500,
+    "elbow": 1650,
+    "wrist": 1500,
+    "rotate": 1400,
+}
+
+# Bottom
+
+bottom_left = {
+    "base": 1620,
+    "shoulder": 1200,
+    "elbow": 1300,
+    "wrist": 1500,
+    "rotate": 1400,
+}
+
+bottom_center = {
+    "base": 1550,
+    "shoulder": 1200,
+    "elbow": 1300,
+    "wrist": 1500,
+    "rotate": 1400,
+}
+
+bottom_right = {
+    "base": 1430,
+    "shoulder": 1200,
+    "elbow": 1300,
+    "wrist": 1500,
+    "rotate": 1400,
+}
